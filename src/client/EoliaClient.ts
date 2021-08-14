b07e4157b37b3eeecff7a02bc93264a9fc84f2d1
Shunichiro Aki <register.develop@nana4.net>
2021-08-14 14:58:58 +0900

    gzipヘッダーを追加

--- src/client/EoliaClient.ts
+++ src/client/EoliaClient.ts
@@ -19,6 +19,7 @@ export class EoliaClient {
         'Accept': 'application/json',
         'Content-Type': 'application/json; charset=UTF-8',
         'Accept-Language': 'ja-jp',
+        'Accept-Encoding': 'gzip'
       },
     };
 
